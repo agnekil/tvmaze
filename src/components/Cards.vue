62f36be0cf5221a3513989f72c8a2ca48f733728
Nathalie Agnekil <nathalie@agnekil.com>
2018-12-11 13:14:07 +0100

    added som styling to cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,9 +1,10 @@
 <template>
 <div class="card-wrapper">
     <div class="card" v-for="show in TvShows" :key="show.show.id">
-    <img :src="show.show.image.medium" alt="poster">
-        <div class="card-image"></div>
-        <div class="card-content">{{show.show.name}}</div>
+        <img :src="show.show.image.medium" alt="poster">
+        <div class="card-content">
+            <h1>{{show.show.name}}</h1>
+        </div>
     </div>
     </div>
 </template>
@@ -38,16 +39,24 @@ export default {
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
 }
 
 .card {
     background-color: rgb(235, 235, 235);
-    height: 350px;
-    width: 220px;
+    height: 400px;
+    width: 200px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 20px;
+    margin: 15px;
+}
+
+h1 {
+    font-size: 20px;
+}
+
+img {
+    width: 200px;
 }
 </style>
